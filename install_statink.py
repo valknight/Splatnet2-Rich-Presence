00860a583135a9d70212ab9a257775cde21a7e7a
Valerokai <vallerie.knight@gmail.com>
2018-11-30 12:19:12 +0000

    Update installer to work with new versions of statink

--- install_statink.py
+++ install_statink.py
@@ -1,23 +1,39 @@
 import os
 import sys
 import time
-from shutil import copyfile
+from shutil import copyfile, rmtree
+
+blacklist = ["README.md", "requirements.txt", ".gitignore"]
 print("Note: if you aren't using venv to run this, you may need to run with admin/root to install python dependencies")
 choice = input("Do you have Git installed on your system? (y/N)")
 
 if choice.lower() == "y":
-	print("Cloning splatnet2statink")
-	print("Source code avaliable at https://github.com/frozenpandaman/splatnet2statink")
-	os.system('git clone https://github.com/frozenpandaman/splatnet2statink.git')
-	os.system('{} -m pip install -r splatnet2statink/requirements.txt'.format(sys.executable))
-	os.system('{} -m pip install -r requirements.txt'.format(sys.executable))
-	print("Copying files into right places")
-	copyfile("splatnet2statink/dbs.py", "dbs.py")
-	copyfile("splatnet2statink/iksm.py", "iksm.py")
-	copyfile("splatnet2statink/splatnet2statink.py", "splatnet2statink.py")
-	print("Installed!")
-	
-else:
-	print("Please install Git then come back here.")
-	time.sleep(3)
+    print("Cloning splatnet2statink")
+    rmtree("splatnet2statink", ignore_errors=True)
+    print("Source code avaliable at https://github.com/frozenpandaman/splatnet2statink")
+    os.system('git clone https://github.com/frozenpandaman/splatnet2statink.git')
+    os.system(
+        '{} -m pip install -r splatnet2statink/requirements.txt --user'.format(sys.executable))
+    os.system('{} -m pip install -r requirements.txt --user'.format(sys.executable))
+    print("Copying files into right places")
+    to_copy = os.listdir("splatnet2statink")
+    print(to_copy)
+    for file in to_copy:
+        file_current = os.path.join("splatnet2statink", file)
+        try:
+            if file not in blacklist:
+                try:
+                    os.remove(file)
+                except FileNotFoundError:
+                    pass
+                copyfile(file_current, "./{}".format(file))
+                print("Copied {}".format(file))
+        except IsADirectoryError:
+            pass
+    print("All files copied, cleaning up...")
+    rmtree("splatnet2statink", ignore_errors=True)
 
+
+else:
+    print("Please install Git then come back here.")
+    time.sleep(3)
