1ad92cf70c9c9e676693f1c18a96de52810fa0dc
HHK3 <joeltilon@live.nl>
2019-07-28 19:26:55 +0200

    Huge Update (User can choose to Auto/Manual Generate Cookie - Auto Cookie Generation Support)

---
+++ config_functions.py
@@ -0,0 +1,39 @@
+import json
+import time
+
+def write_config(tokens):
+	'''Writes config file and updates the global variables.'''
+
+	config_file = open("config/config.txt", "w")
+	config_file.seek(0)
+	config_file.write(json.dumps(tokens, indent=4,
+                              sort_keys=True, separators=(',', ': ')))
+	config_file.close()
+
+	config_file = open("config/config.txt", "r")
+	config_data = json.load(config_file)
+
+	global YOUR_METHOD
+	YOUR_METHOD = config_data["method"]
+	global SESSION_TOKEN
+	SESSION_TOKEN = config_data["session_token"]
+	global YOUR_COOKIE
+	YOUR_COOKIE = config_data["cookie"]
+	global USER_LANG
+	USER_LANG = config_data["user_lang"]
+
+	config_file.close()
+
+
+def get_config_file():
+    '''Get the data from the config file, and create it if not present'''
+    try:
+        with open("config/config.txt") as config_f:
+            config_data = json.loads(config_f.read())
+    except FileNotFoundError:
+        config_to_write = {"cookie": "", "friend_code": "",
+                           "method": "",  "session_token": "", "user_lang": "en-US"}
+        with open("config/config.txt", "w") as config_f:
+            config_f.write(json.dumps(config_to_write, indent=4))
+        config_data = get_config_file()
+    return config_data
